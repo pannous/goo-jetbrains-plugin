0a5ff9db5af3f02eb028fe7be1a019d475a8fd40
pannous <info@pannous.com>
2025-07-24 22:35:29 +0200

    build.gradle.kts GOOD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.kotlin.jvm") version "2.2.0"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
 }
 
 group = "com.pannous"
-version = "1.1.1"
+version = "1.2.1"
 
 repositories {
     mavenCentral()
@@ -21,41 +21,22 @@ intellijPlatform {
             untilBuild = "251.*"
         }
     }
-    
-    sandboxContainer = file("${rootProject.projectDir}/.sandbox")
 }
 
 dependencies {
     intellijPlatform {
-        goland("2025.1.3")
-        instrumentationTools()
-        pluginVerifier()
-        zipSigner()
-        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+        local("/Applications/GoLand.app/Contents")
     }
 }
 
 tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "21"
-    }
+//    patchPluginXml {
+//        sinceBuild.set("251")
+//        untilBuild.set("251.*")
+//    }
 
-    patchPluginXml {
-        sinceBuild.set("251")
-        untilBuild.set("251.*")
-    }
-    
-    buildSearchableOptions {
-        enabled = false
-    }
-    
     runIde {
-        jvmArgs = listOf("-Xmx2048m", "-XX:+UseG1GC")
         systemProperty("idea.auto.reload.plugins", "true")
-        systemProperty("idea.plugin.in.sandbox.mode", "true")
+//        systemProperty("idea.plugin.in.sandbox.mode", "false")
     }
 }
